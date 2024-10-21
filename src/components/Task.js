848c3236a969f311ab698b7ae238efd2411d3480
Anton Hansson <125166515+antxhan@users.noreply.github.com>
2024-10-21 18:24:32 +0200

    basic task rendering

---
+++ src/components/Task.js
@@ -0,0 +1,62 @@
+import placeholderIcon from "../assets/images/placeholder.png";
+import duplicateIcon from "../assets/icons/copy.svg";
+import deleteIcon from "../assets/icons/trash.svg";
+
+export default function Task(task) {
+  const container = document.createElement("div");
+  container.className = "task";
+
+  //   title
+  const titleContainer = document.createElement("div");
+  titleContainer.className = "task__title";
+  const titleIcon = document.createElement("img");
+  titleIcon.src = placeholderIcon;
+  titleIcon.alt = "placeholder image";
+  titleContainer.appendChild(titleIcon);
+  const titleText = document.createElement("h3");
+  titleText.textContent = task.title;
+  titleContainer.appendChild(titleText);
+
+  // description
+  const description = document.createElement("p");
+  description.className = "task__description";
+  description.textContent = task.description;
+
+  // status
+  let status;
+  if (!task.subtasks || task.subtasks.length === 0) {
+    status = document.createElement("input");
+    status.className = "task__status";
+    status.type = "checkbox";
+  } else {
+    status = document.createElement("div");
+    status.className = "task__status";
+    status.textContent = "26%";
+  }
+
+  // buttons
+  const buttonsContainer = document.createElement("div");
+  buttonsContainer.className = "task__buttons";
+
+  // duplicate button
+  const duplicateButton = document.createElement("button");
+  const duplicateButtonIcon = document.createElement("img");
+  duplicateButtonIcon.src = duplicateIcon;
+  duplicateButtonIcon.alt = "duplicate";
+  duplicateButton.appendChild(duplicateButtonIcon);
+  buttonsContainer.appendChild(duplicateButton);
+
+  // delete button
+  const deleteButton = document.createElement("button");
+  const deleteButtonIcon = document.createElement("img");
+  deleteButtonIcon.src = deleteIcon;
+  deleteButtonIcon.alt = "delete";
+  deleteButton.appendChild(deleteButtonIcon);
+  buttonsContainer.appendChild(deleteButton);
+
+  container.appendChild(titleContainer);
+  container.appendChild(description);
+  container.appendChild(status);
+  container.appendChild(buttonsContainer);
+  return container;
+}
--- src/index.js
+++ src/index.js
@@ -1,13 +1,43 @@
-import Projects from "./pages/projects";
+import TaskElement from "./components/task";
 import "./styles/global.css";
+import placeholderIcon from "./assets/images/placeholder.png";
+
+class App {
+  constructor(tasks = []) {
+    this._tasks = tasks;
+  }
+  get tasks() {
+    return this._tasks;
+  }
+  addTask(task) {
+    this._tasks.push(task);
+  }
+  deleteTask(task) {
+    this._tasks.splice(this._tasks.indexOf(task), 1);
+  }
+}
 
 class Task {
-  constructor(title, description, icon, dueDate, priority, complete = false) {
+  constructor(
+    title = "Untitled", // string
+    description = "", // string
+    icon = placeholderIcon, // string
+    dueDate = "", // string
+    priority = 0 // number
+  ) {
+    this._id = "";
     this._title = title;
     this._description = description;
+    this._icon = icon;
+    this._createdAt = new Date();
+    this._updatedAt = this._createdAt;
     this._dueDate = dueDate;
     this._priority = priority;
-    this._complete = complete;
+    this._subtasks = null; // array or null
+    this._complete = false; // boolean, based on subtasks completedness
+  }
+  get id() {
+    return this._id;
   }
   get title() {
     return this._title;
@@ -26,6 +56,12 @@ class Task {
   }
   set icon(icon) {
     this._icon = icon;
+  }
+  get createdAt() {
+    return this._createdAt;
+  }
+  get updatedAt() {
+    return this._createdAt;
   }
   get dueDate() {
     return this._dueDate;
@@ -42,71 +78,64 @@ class Task {
   get complete() {
     return this._complete;
   }
+  set complete(bool) {
+    this._complete = bool;
+  }
   toggleComplete() {
     this._complete = !this._complete;
   }
-}
-
-class Project {
-  constructor(title, description, icon, tasks = []) {
-    this._title = title;
-    this._description = description;
-    this._icon = icon;
-    this._tasks = tasks;
+  get subtasks() {
+    return this._subtasks;
   }
-  get title() {
-    return this._title;
+  addSubtask(subtask) {
+    this._subtasks.push(subtask);
   }
-  set title(title) {
-    this._title = title;
-  }
-  get description() {
-    return this._description;
-  }
-  set description(description) {
-    this._description = description;
-  }
-  get icon() {
-    return this._icon;
-  }
-  set icon(icon) {
-    this._icon = icon;
-  }
-  get tasks() {
-    return this._tasks;
-  }
-  addTask(task) {
-    this.tasks.push(task);
-  }
-  deleteTask(task) {
-    this.tasks.splice(this.tasks.indexOf(task), 1);
-  }
-}
-
-class App {
-  constructor() {
-    this._projects = [];
-  }
-  get projects() {
-    return this._projects;
-  }
-  addProject(project) {
-    this.projects.push(project);
-  }
-  deleteProject(project) {
-    this.projects.splice(this.projects.indexOf(project), 1);
+  deleteSubtask(subtask) {
+    this._subtasks.splice(this._subtasks.indexOf(subtask), 1);
   }
 }
 
 class View {
-  constructor() {}
+  constructor() {
+    this._tasks = document.querySelector(".tasks");
+    this.renderTasks();
+    this.init();
+  }
+  init() {
+    document
+      .querySelector(".main-header__new-button")
+      .addEventListener("click", (e) => {
+        e.preventDefault();
+        const newTaskDialog = document.querySelector(".new-task-dialog");
+        newTaskDialog.showModal();
+
+        // validate form data
+        //
+
+        // create new Task from data
+        const task = new Task();
+
+        // add new Task to app.tasks
+        //
+        // render view
+        //
+      });
+  }
+  renderTasks() {
+    // reset tasks
+    this._tasks.innerHTML = "";
+
+    // render tasks
+    app.tasks.forEach((task) => {
+      const taskElement = TaskElement(task);
+      this._tasks.appendChild(taskElement);
+    });
+  }
 }
 
 const app = new App();
-const defaultProject = new Project("Untitled", "My first project.");
-app.addProject(defaultProject);
+const defaultProject = new Task("First project", "This is my first project.");
+app.addTask(defaultProject);
+// console.log(app.tasks);
 
-console.log(app.projects);
-
-const thing = document.querySelector("body");
-thing.appendChild(Projects());
+const view = new View();
