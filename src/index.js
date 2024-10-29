78e0de12e517bf1de5d9dee2d66a8cea00c2bdd5
Anton Hansson <125166515+antxhan@users.noreply.github.com>
2024-10-29 16:08:19 +0100

    feat: handle subtask click navigation

--- src/index.js
+++ src/index.js
@@ -1,256 +1,130 @@
-import TaskElement from "./components/task";
 import "./styles/global.css";
-import placeholderIcon from "./assets/images/placeholder.png";
 
 class Task {
   constructor(
-    title = "Untitled", // string
-    description = "", // string
-    icon = placeholderIcon, // string
-    dueDate = "", // string
-    priority = 0 // number
+    title,
+    description = "",
+    dueDate = null,
+    completed = false,
+    parentTask = null,
+    subtasks = [],
+    id = new Date()
   ) {
-    this._id = "";
-    this._title = title;
-    this._description = description;
-    this._icon = icon;
-    this._createdAt = new Date();
-    this._updatedAt = this._createdAt;
-    this._dueDate = dueDate;
-    this._priority = priority;
-    this._parentTask = null; // Task || null
-    this._subtasks = null; // array || null
-    this._complete = false; // boolean
+    this.title = title;
+    this.description = description;
+    this.dueDate = dueDate;
+    this.completed = completed;
+    this.parentTask = parentTask;
+    this.subtasks = subtasks;
+    this.id = id;
   }
-  get id() {
-    return this._id;
+  addSubtask(task) {
+    this.subtasks.push(task);
   }
-  get title() {
-    return this._title;
+  deleteSubtask(taskId) {
+    this.subtasks = this.subtasks.filter((task) => task.id !== taskId);
   }
-  set title(title) {
-    this._title = title;
-  }
-  get description() {
-    return this._description;
-  }
-  set description(description) {
-    this._description = description;
-  }
-  get icon() {
-    return this._icon;
-  }
-  set icon(icon) {
-    this._icon = icon;
-  }
-  get createdAt() {
-    return this._createdAt;
-  }
-  get updatedAt() {
-    return this._createdAt;
-  }
-  get dueDate() {
-    return this._dueDate;
-  }
-  set dueDate(dueDate) {
-    this._dueDate = dueDate;
-  }
-  get priority() {
-    return this._priority;
-  }
-  set priority(priority) {
-    this._priority = priority;
-  }
-  get complete() {
-    return this._complete;
-  }
-  set complete(bool) {
-    this._complete = bool;
-    if (this.parentTask) {
-      this.parentTask.calculateCompletedSubtasks();
-    }
-  }
-  calculateCompletedSubtasks() {
-    const completedCount = this.subtasks.filter(
-      (subtask) => subtask.complete
-    ).length;
-    const totalCount = this.subtasks.length;
-    if (completedCount === totalCount) {
-      this.complete = true;
-    }
-    const completionPercentage = (completedCount / totalCount) * 100;
-    return completionPercentage;
-  }
-  get parentTask() {
-    return this._parentTask;
-  }
-  set parentTask(parentTask) {
-    this._parentTask = parentTask;
-  }
-  get subtasks() {
-    return this._subtasks;
-  }
-  set subtasks(subtasks) {
-    this._subtasks = subtasks;
-  }
-  delete() {
-    console.log("i was called");
-    this.parentTask.deleteSubtask(this);
-  }
-  addSubtask(subtask) {
-    subtask.parentTask = this;
-    if (!this.subtasks) {
-      this.subtasks = [];
-    }
-    this.subtasks.push(subtask);
-  }
-  deleteSubtask(subtask) {
-    this.subtasks.splice(this.subtasks.indexOf(subtask), 1);
+  getSubtasks() {
+    return this.subtasks;
   }
 }
 
 class View {
-  constructor(task) {
-    this._currentTask = task;
-    this._currentTab = document.querySelector(
-      ".main-header__tabs button[aria-current='true']"
-    );
-    this._tasks = document.querySelector(".tasks");
-    this.renderTasks();
-    this.init();
+  constructor() {
+    this.task = undefined;
+    this.currentTab = "ongoing";
+    this.taskHeader = document.querySelector(".main-header");
+    this.taskTitleContainer = document.querySelector(".task__title");
+    this.taskDescriptionContainer =
+      document.querySelector(".task__description");
+    this.subtasksContainer = document.querySelector(".subtasks");
   }
-  get currentTask() {
-    return this._currentTask;
+  render(task) {
+    this.task = task;
+    this.renderTitle();
+    this.renderDescription();
+    this.renderSubtasks();
   }
-  set currentTask(task) {
-    this._currentTask = task;
+  renderTitle() {
+    this.taskTitleContainer.innerHTML = this.task.title;
   }
-  get currentTab() {
-    return this._currentTab;
-  }
-  set currentTab(currentTab) {
-    this._currentTab = currentTab;
-  }
-  get tasks() {
-    return this._tasks;
-  }
-  set tasks(tasks) {
-    this._tasks = tasks;
-  }
-  init() {
-    // new task button
-    const newTaskDialog = document.querySelector(".new-task-dialog");
-    document
-      .querySelector(".main-header__new-button")
-      .addEventListener("click", (e) => {
-        newTaskDialog.showModal();
-      });
-
-    // handle new task dialog
-    newTaskDialog.querySelectorAll("button").forEach((button) => {
-      button.addEventListener("click", (e) => {
-        // e.preventDefault();
-
-        if (button.type === "submit") {
-          const titleInput = newTaskDialog.querySelector("input[name=title]");
-          const descriptionInput = newTaskDialog.querySelector(
-            "input[name=description]"
-          );
-          const dueDateInput = newTaskDialog.querySelector(
-            "input[name=dueDate]"
-          );
-          const prioritySelect = newTaskDialog.querySelector(
-            "select[name=priority]"
-          );
-
-          // validate form data
-          if (newTaskDialog.querySelector("form").checkValidity()) {
-            // create new Task from data
-            const task = new Task(
-              titleInput.value,
-              descriptionInput.value,
-              dueDateInput.value,
-              prioritySelect.value
-            );
-
-            // add new Task to currentTask.tasks
-            this.currentTask.addSubtask(task);
-
-            // close
-            newTaskDialog.close();
-            newTaskDialog.querySelector("form").reset();
-          }
-        } else {
-          // close
-          newTaskDialog.close();
-          newTaskDialog.querySelector("form").reset();
-        }
-
-        // render view
-        this.renderTasks();
-      });
-    });
-
-    // tabs
-    const tabs = Array.from(
-      document.querySelector(".main-header__tabs").children
-    );
-    tabs.forEach((tab) => {
-      tab.addEventListener("click", (e) => {
-        // reset tab highlight
-        tabs.forEach((tab) => {
-          tab.setAttribute("aria-current", "false");
-        });
-        e.currentTarget.setAttribute("aria-current", "true");
-        this.currentTab = e.currentTarget;
-        this.renderTasks();
-      });
-    });
-
-    // hiding context menu
-    document.addEventListener("click", (e) => {
-      this.hideContextMenu();
-    });
-    window.addEventListener("resize", () => {
-      this.hideContextMenu();
-    });
-  }
-  hideContextMenu() {
-    const contextMenu = document.querySelector(".context-menu");
-    if (contextMenu) {
-      contextMenu.setAttribute("aria-visible", "false");
-      contextMenu.removeAttribute("style");
+  renderDescription() {
+    this.taskDescriptionContainer.style.display = "block";
+    this.taskDescriptionContainer.innerHTML = this.task.description;
+    if (!this.task.description) {
+      this.taskDescriptionContainer.style.display = "none";
     }
   }
-  renderTasks() {
-    // reset
-    this.tasks.innerHTML = "";
-
-    // render tasks
-    this.currentTask.subtasks.forEach((task) => {
-      if (this.currentTab.value === "ongoing" && !task.complete) {
-        const taskElement = TaskElement(task);
-        this.tasks.appendChild(taskElement);
-      } else if (this.currentTab.value === "archive" && task.complete) {
-        const taskElement = TaskElement(task);
-        this.tasks.appendChild(taskElement);
-      }
+  renderSubtasks() {
+    if (!this.task.subtasks) {
+      return;
+    }
+    this.subtasksContainer.innerHTML = "";
+    this.subtasksContainer.innerHTML = `
+      ${this.task.subtasks
+        .map(
+          (subtask) => `
+        <li class="subtask">
+          <h3 class="subtask__title">${subtask.title}</h3>
+        ${
+          subtask.subtasks.length > 0
+            ? `<p class="subtask__status">${
+                (subtask.subtasks.reduce(
+                  (acc, subtask) => acc + subtask.completed,
+                  0
+                ) /
+                  subtask.subtasks.length) *
+                100
+              }%</p>`
+            : `<input class="subtask__status" type="checkbox" ${
+                subtask.completed ? "checked" : ""
+              } >`
+        }
+        </li>
+        `
+        )
+        .join("")}
+    `;
+  }
+  bindClickSubtask(handler) {
+    const subtasks = this.subtasksContainer.querySelectorAll(".subtask");
+    subtasks.forEach((subtask, index) => {
+      subtask.addEventListener("click", (e) => handler(e, index));
     });
   }
 }
 
-const origin = new Task();
+class Controller {
+  constructor(task, view) {
+    this.task = task;
+    this.view = view;
+    this.updateView();
+  }
+  updateView() {
+    this.view.render(this.task);
+    this.view.bindClickSubtask(this.handleClickSubtask.bind(this));
+  }
+  handleClickSubtask(e, subtaskIndex) {
+    if (e.target.type === "checkbox") {
+      return;
+    }
+    const subtask = this.task.subtasks[subtaskIndex];
+    this.task = subtask;
+    this.updateView();
+  }
+}
 
-const defaultProject = new Task("First project", "This is my first project.");
-origin.addSubtask(defaultProject);
+const task = new Task("Master Task", "", new Date());
 
-const secondProject = new Task("Second project", "This is my second project.");
-const subtask = new Task("Subtask", "This is my subtask.");
-const subtask2 = new Task("subtask 2", "This is my subtask 2.");
-secondProject.addSubtask(subtask);
-secondProject.addSubtask(subtask2);
-subtask.complete = true;
-subtask2.complete = true;
-origin.addSubtask(secondProject);
+task.subtasks = [
+  new Task("task 1", "desc", new Date()),
+  new Task("task 2", "", new Date()),
+];
 
-const view = new View(origin);
+task.subtasks[0].subtasks = [
+  new Task("task 1", "desc", new Date()),
+  new Task("task 2", "desc", new Date(), true),
+];
+
+const view = new View();
+const controller = new Controller(task, view);
